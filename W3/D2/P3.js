c6795e31784cb233e07ec6bf367b4bac3431dff6
Madhukesh-03 <madhukeshprakasha400@gmail.com>
2026-03-03 15:41:23 +0530

    Logical operators

---
+++ W3/D2/P3.js
@@ -0,0 +1,6 @@
+//Logical operators
+console.log("Logical operators");
+const a=true, b=false;
+console.log("a&&b=",a&&b);//logical AND
+console.log("a||b=",a||b);//logical OR
+console.log("!a=",!a);//logical NOT
---
+++ W3/D2/P4.js
@@ -0,0 +1,36 @@
+// //Conditional statements
+// let age =20;
+// if(age<13){
+//     console.log("Child");
+// }
+// else if(age<18){
+//     console.log("Teenager");
+// }
+// else{
+//     console.log("Adult");
+// }
+
+// //Switch statement
+// console.log("Switch statements");
+// const day = "tuesday";
+// switch (day) {
+//     case "Monday":
+//         console.log("Start of the week");
+//         break;
+
+//     case "Wednesday":
+//         console.log("Midweek");
+//         break;
+//     case "Friday":
+//         console.log("End of the work week");
+//         break;
+
+//     default:
+//         console.log("Some day of the week");
+//         break;
+
+// }
+
+//Type conversion 
+let n = Number("ABC");
+console.log(" n:",n," typeof n:", typeof n, "isNan",isNaN(n)); // 123
