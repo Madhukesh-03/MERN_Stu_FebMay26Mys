c308c86069449ff8adc79dec83afca2e9be4dce0
Madhukesh-03 <madhukeshprakasha400@gmail.com>
2026-03-03 15:39:55 +0530

    Arithmetic Operators

---
+++ W3/D2/P1.js
@@ -0,0 +1,16 @@
+//Arithmetic Operators
+console.log("Arithmetic Operators");
+const a=2, b=3;
+const c=a+b;
+console.log("a+b",c);
+console.log("a+b: ",a+b);
+console.log("a-b: ",a-b);
+console.log("a*b: ",a*b);
+console.log("a/b: ",a/b);
+console.log("a%b: ",a%b);//modulus
+console.log("a**b: ",a**b);//Exponential
+// console.log("++a: ",++a);//pre increment
+// console.log("a++: ",a++);//post increment
+// console.log("--a: ",--a);//pre decrement
+// console.log("a--: ",a--);//post decrement 
+  
