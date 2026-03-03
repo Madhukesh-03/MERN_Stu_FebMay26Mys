8d960c4c61303d9aaa04d2a9de9d9b060d3100db
Madhukesh-03 <madhukeshprakasha400@gmail.com>
2026-03-03 15:42:14 +0530

    Conditional statements

--- W3/D2/P4.js
+++ W3/D2/P4.js
@@ -1,35 +1,35 @@
-// //Conditional statements
-// let age =20;
-// if(age<13){
-//     console.log("Child");
-// }
-// else if(age<18){
-//     console.log("Teenager");
-// }
-// else{
-//     console.log("Adult");
-// }
+//Conditional statements
+let age =20;
+if(age<13){
+    console.log("Child");
+}
+else if(age<18){
+    console.log("Teenager");
+}
+else{
+    console.log("Adult");
+}
 
-// //Switch statement
-// console.log("Switch statements");
-// const day = "tuesday";
-// switch (day) {
-//     case "Monday":
-//         console.log("Start of the week");
-//         break;
+//Switch statement
+console.log("Switch statements");
+const day = "tuesday";
+switch (day) {
+    case "Monday":
+        console.log("Start of the week");
+        break;
 
-//     case "Wednesday":
-//         console.log("Midweek");
-//         break;
-//     case "Friday":
-//         console.log("End of the work week");
-//         break;
+    case "Wednesday":
+        console.log("Midweek");
+        break;
+    case "Friday":
+        console.log("End of the work week");
+        break;
 
-//     default:
-//         console.log("Some day of the week");
-//         break;
+    default:
+        console.log("Some day of the week");
+        break;
 
-// }
+}
 
 //Type conversion 
 let n = Number("ABC");
