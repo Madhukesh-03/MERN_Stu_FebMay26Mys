71958fab149e4dafa0e0cb0b4be98ee9d3c42162
Madhukesh-03 <madhukeshprakasha400@gmail.com>
2026-03-04 14:25:46 +0530

    Break,Continue statements

---
+++ W3/D3/P3.js
@@ -0,0 +1,16 @@
+for(let i=0;i<5;i++){ //forward loop
+    if(i===2){
+    console.log(i);
+    break
+    }
+    
+}
+
+//Continue statement
+for(let i=0;i<5;i++){ //forward loop
+    if(i===2){
+    // console.log(i);
+    continue;
+    }
+    console.log(i);
+}
