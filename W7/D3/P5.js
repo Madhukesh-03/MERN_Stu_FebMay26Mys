6323d90f463f683d0a0054814b6717100fce26d9
Madhukesh-03 <madhukeshprakasha400@gmail.com>
2026-04-01 12:33:28 +0530

    JWT Fundamentals:token generated and verification

---
+++ W7/D3/P5.js
@@ -0,0 +1,36 @@
+//JWT Fundamentals:token generated and verification
+const jwt=require("jsonwebtoken")
+
+const secretKey="monkey123"
+
+//payload holds small non sensitive data
+const payload={
+    userId:101,
+    role:"member"
+};
+
+//jwt.sign() creates a signes JWT token
+const token=jwt.sign(payload,secretKey,{expiresIn:"1h"});
+
+console.log("Token generated successfully\n",token);
+
+
+const tokenParts = token.split(".");
+console.log("Header section.",tokenParts[0]);
+console.log("Payload section.",tokenParts[1]);
+console.log("Signature section.",tokenParts[2]);
+console.log("JWT part count.",tokenParts.length);
+;
+
+try{
+    //jwt.verify() checks trust, signature and expiration
+    const verifiedPayload = jwt.verify(token,secretKey);
+
+    console.log("Verified Payload",verifiedPayload);
+}
+catch{
+    console.log("verification failed",error.message);
+}
+
+const decodeWithoutVerification = jwt.decode(token);
+console.log("decoded token:",decodeWithoutVerification);
