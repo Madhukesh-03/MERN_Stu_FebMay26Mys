402e76ee8397d56cb459e2d1e6edb7a5c38b0e9c
Madhukesh-03 <madhukeshprakasha400@gmail.com>
2026-04-06 17:39:29 +0530

    Assigning roles to users and restricting access

---
+++ W7/D5/P5.js
@@ -0,0 +1,49 @@
+//Assigning roles to users and restricting access
+const express = require("express");
+const app = express();
+
+app.use(function(req,res,next){
+    req.user = {
+        id:101,
+        username:"Madhukesh",
+        role:"user"
+    };
+    next();         
+});
+
+function requireRole(role){
+    return function(req,res,next){
+        if(!req.user){
+            return res.status(401).json({
+                success:false,
+                message:"Authentication required"
+        });
+    }
+        if(req.user.role !== role){
+            return res.status(403).json({
+                success:false,
+                message:"Access denied: insufficient permissions"
+            });
+        }
+        next();
+    }
+}
+
+app.get("/profile",function(req,res){
+    res.json({
+        success:true,
+        message:"Profile Page",
+        user:req.user
+    });
+});
+
+app.get("/admin",requireRole("admin"),function(req,res){
+    res.json({
+        success:true,
+        message:"Profile Page",
+        user:req.user
+    });
+});
+app.listen(4000, function () {
+    console.log("Express-session demo server running at http://localhost:4000");
+});
